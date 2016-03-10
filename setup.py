bf74c10d75d0ec6d89857908cee322d224cc2a05
bmoar <ben@rooted.systems>
2016-03-10 07:25:49 -0500

    rm install_requires

--- setup.py
+++ setup.py
@@ -3,45 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='flask_truss',
-    version='0.0.1',
+    name='python_truss',
+    version='1.0.0',
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    package_data={
-        'templates': 'flask_truss/templates/*',
-        'static': 'flask_truss/static/*',
-        'scripts': 'flask_truss/scripts/migrations/*'
-    },
     install_requires=[
-        'celery',
         'coverage',
-        'Flask-Admin',
-        'Flask-Debugtoolbar',
-        'Flask-Login',
-        'Flask-Marshmallow',
-        'Flask-Migrate',
-        'Flask-Script',
-        'Flask-SQLAlchemy',
-        'Flask-Testing',
-        'Flask-WTF',
-        'Flask',
-        'ipdb',
-        'ipython',
-        'marshmallow-sqlalchemy',
-        'nose',
-        'paramiko',
-        'passlib',
-        'psycopg2',
-        'python-dateutil',
-        'pytz',
-        'randomize',
-        'SQLAlchemy',
-        'statsd',
-        'Werkzeug',
     ],
-    tests_require=['nose'],
-    test_suite='nose.collector',
     classifiers=[
         'Private :: Do Not Upload'
     ],
