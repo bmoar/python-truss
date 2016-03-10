c7ebd90929823d24feb6ae8444addd6a3f36360d
bmoar <ben@rooted.systems>
2016-03-10 07:41:44 -0500

    add docstring for python_truss

--- python_truss/cli/python_truss.py
+++ python_truss/cli/python_truss.py
@@ -1,6 +1,7 @@
 ''' a skeleton cli program to begin development with '''
 
 def main():
+    ''' entry point for console_scripts, and cli binary '''
     print("hello world!")
     return 0
 
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,4 @@
 from python_truss.cli.python_truss import main
 
-
 def test_func():
     assert main() == 0
